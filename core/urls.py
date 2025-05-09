c37024f3a5d7c823d4c3a715ac836b6375d68082
Azimjonov Islomjon <azimjonovislomjon77@gmail.com>
2025-05-10 04:06:26 +0500

    end

--- core/urls.py
+++ core/urls.py
@@ -19,13 +19,35 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from core import settings
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Exam7 API",
+      default_version='v1',
+      description="API documentation for Exam7 project",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@exam7.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-                path('admin/', admin.site.urls),
-                path('course/', include('course.urls', namespace='course')),
-                path('users/', include('users.urls', namespace='users')),
-                path('social-auth/', include('social_django.urls', namespace='social')),
-                path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-                path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Your other URLs here
+    path('admin/', admin.site.urls),
+    path('course/', include('course.urls', namespace='course')),
+    path('users/', include('users.urls', namespace='users')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+
+    # Swagger Docs
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-docs'),
+
+    # Token auth
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

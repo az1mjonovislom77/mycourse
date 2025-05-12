a2f160cb0abf64650e75c6eccb055f800981839d
Azimjonov Islomjon <azimjonovislomjon77@gmail.com>
2025-05-12 19:48:56 +0500

    end

--- core/settings.py
+++ core/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mycoursedb',          
-        'USER': 'postgres',        
-        'PASSWORD': '7777777', 
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get("POSTGRES_DB", "mycoursedb"),          
+        'USER': os.environ.get("POSTGRES_USER", "postgres"),        
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "7777777"), 
+        'HOST': os.environ.get("POSTGRES_HOST", "localhost"),
+        'PORT': os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
